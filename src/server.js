5e3f2a917879593b9a9de9ced56cc7e7b6705efb
codingfrank <codingfrank73@gmail.com>
2022-11-02 12:27:07 +0100

    add the .env value2

--- src/server.js
+++ src/server.js
@@ -12,16 +12,18 @@ const PORT = process.env.PORT || 9000;
 // Body-Parser -> *** WICHTIG ***
 app.use(express.json())
 
+let test = process.env.NODE_TEST;
 
 
 app.use(cors({ origin: true, credentials: true }))
 
 app.get('/', (req, res) => {
-    res.send('Herzlich willkommen auf meinem ersten WEB-App Azure-Server mit Git-Hub Anbindung...')
+    res.send(`Herzlich willkommen auf meinem ersten WEB-App Azure-Server mit Git-Hub Anbindung: ${test}`)
 })
 
 app.use("/api/users", userRouter)
 
 app.listen(PORT, () => {
     console.log(`Server listen on Port: ${PORT}`);
+    console.log(test);
 })
